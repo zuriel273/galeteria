91dcbf005d31b351b6299d59a2a5fbe1b864d9d0
Arlindo Jr <ba.elias@gmail.com>
2013-10-08 22:41:29 -0300

    Cadastrando Pedido

--- src/negocio/Pedido.java
+++ src/negocio/Pedido.java
@@ -9,32 +9,78 @@ package negocio;
  * @author Arlindo
  */
 public class Pedido {
-    String  descricao;
+    String  pedido1, pedido2, pedido3, pedido4, pedido5, pedido6, pedido7, pedido8;
+    String  endereco, complemento;
     boolean entregue;
-    float valor;
     int id;
     Cliente cliente;
 
-    public Pedido(String descricao, float valor, Cliente cliente) {
-        this.descricao = descricao;
-        this.valor = valor;
+    public Pedido(Cliente cliente) {
         this.cliente = cliente;
     }
 
-    public float getValor() {
-        return valor;
+    public String getPedido1() {
+        return pedido1;
     }
 
-    public void setValor(float valor) {
-        this.valor = valor;
+    public void setPedido1(String pedido1) {
+        this.pedido1 = pedido1;
     }
 
-    public String getDescricao() {
-        return descricao;
+    public String getPedido2() {
+        return pedido2;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public void setPedido2(String pedido2) {
+        this.pedido2 = pedido2;
+    }
+
+    public String getPedido3() {
+        return pedido3;
+    }
+
+    public void setPedido3(String pedido3) {
+        this.pedido3 = pedido3;
+    }
+
+    public String getPedido4() {
+        return pedido4;
+    }
+
+    public void setPedido4(String pedido4) {
+        this.pedido4 = pedido4;
+    }
+
+    public String getPedido5() {
+        return pedido5;
+    }
+
+    public void setPedido5(String pedido5) {
+        this.pedido5 = pedido5;
+    }
+
+    public String getPedido6() {
+        return pedido6;
+    }
+
+    public void setPedido6(String pedido6) {
+        this.pedido6 = pedido6;
+    }
+
+    public String getPedido7() {
+        return pedido7;
+    }
+
+    public void setPedido7(String pedido7) {
+        this.pedido7 = pedido7;
+    }
+
+    public String getPedido8() {
+        return pedido8;
+    }
+
+    public void setPedido8(String pedido8) {
+        this.pedido8 = pedido8;
     }
 
     public boolean isEntregue() {
@@ -61,4 +107,21 @@ public class Pedido {
         this.cliente = cliente;
     }
 
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    
+    
 }
